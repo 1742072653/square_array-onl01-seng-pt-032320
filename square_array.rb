e0116eb952d49960fe067131e3f6006b069dcd28
Wenhao Guo <gwh1742072653@gmail.com>
2020-03-27 18:49:26 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,8 +1,10 @@
 def square_array(arr)
   count = 0
   new_numbers = []
+  
   arr.each do |num|
-    new_numbers[count] = #{num} * #{num}
+    new_numbers[count] = 
   end
+  
   return new_numbers
 end
